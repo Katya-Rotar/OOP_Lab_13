38fe82373d006e3322070b49264ee7db23bc9748
user <katya.rotar02@gmail.com>
2023-11-06 20:43:27 +0200

    Task_4

---
+++ Task_4/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string word = Console.ReadLine();
+        string[] sentences = Console.ReadLine().Split('.', '!','?');
+        for(int i = 0; i < sentences.Length; i++)
+        {
+            string[] sentence = sentences[i].Split(' ', ',');
+            for(int j = 0; j < sentence.Length; j++)
+            {
+                if (sentence[j] == word) {
+                    Console.WriteLine(sentences[i]);
+                }
+            }
+        }
+    }
+}
