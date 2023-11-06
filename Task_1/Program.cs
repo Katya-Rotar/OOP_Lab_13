6f828d794cf1a90052b18c06f1932df74e19b7f2
user <katya.rotar02@gmail.com>
2023-11-06 19:09:42 +0200

    Add project files.

---
+++ Task_1/Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+        string reversesInput = Reverses(input);
+        Console.WriteLine(reversesInput);
+    }
+    public static string Reverses(string input) {
+        char[] chars = input.ToCharArray();
+        Array.Reverse(chars);
+        return new string(chars);
+    }
+}
